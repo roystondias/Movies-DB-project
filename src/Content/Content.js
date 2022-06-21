f55ad83f71ec07ceec2ac30c2dca98c5b052c80b
royston <diasroyston034@gmail.com>
2022-06-21 10:22:44 +0530

    Genre Filter
    implementation of Genre Filter

--- src/Content/Content.js
+++ src/Content/Content.js
@@ -4,28 +4,21 @@ import axios from 'axios'
 
 function Content(props) {
     const [data, setdata] = useState([]);
-    const [genre, setgenre] = useState([]);
     //adding const of the urlink to avoid code duplication
     const API_KEY = "?api_key=8a5c64f212f1e2d52bc655c31e4c9c22";
     const BASE_URL = "https://api.themoviedb.org/3";
     const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w300";
-    let URL = BASE_URL + "/movie/popular" + API_KEY + "&language=en-US&page=1";
+    const GENRE_LIST = props.genreData;
+    let URL = BASE_URL + "/movie/popular" + API_KEY + "&language=en-US&page=1&with_genres=" + GENRE_LIST;
 
     // useEffect hook runs onces in which the GET request is made for the popular movies URL
     useEffect(() => {
         axios.request(URL).then(response => {
             console.log(response.data.results);
+            console.log(URL);
             setdata(response.data.results);
         });
-    }, [])
-
-    //useEffect hook runs once to get the JSON file of the genre list
-    useEffect(() => {
-        axios.request(`https://api.themoviedb.org/3/genre/movie/list${API_KEY}&language=en-US`).then(response => {
-            console.log(response.data.genres);
-            setgenre(response.data.genres);
-        });
-    }, [])
+    }, [URL])
 
     console.log(props.genreData);
     return (
