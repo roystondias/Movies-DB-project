e033383bef44d2523d54c6780b608fabc536f970
royston <diasroyston034@gmail.com>
2022-06-22 11:19:24 +0530

    pagination to content

--- src/Content/Content.js
+++ src/Content/Content.js
@@ -23,23 +23,34 @@ function Content(props) {
     console.log(props.genreData);
     return (
         <>
-        <br />
-        <div className='container'>
-            <div className="row">
-                {data.map(output_data => {
-                    return <div className="col-3">
-                        <div className="card" style={{ width: "18rem" }}>
-                            <img src={`${IMAGE_BASE_URL}${output_data.poster_path}`} className="card-img-top" alt="..." />
-                            <div className="card-body">
-                                <h5 className="card-title">{output_data.original_title}</h5>
-                                <p className="card-text">{output_data.overview}</p>
-                                <a href="/" className="btn btn-primary">Read More</a>
+            <br />
+            <div className='container'>
+                <div className="row my-2">
+                    {data.map(output_data => {
+                        return <div className="col-3">
+                            <div className="card" style={{ width: "18rem" }}>
+                                <img src={`${IMAGE_BASE_URL}${output_data.poster_path}`} className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title my-2">{output_data.original_title}</h5>
+                                    <p className="card-text">{output_data.overview}</p>
+                                    <a href={`https://www.themoviedb.org/movie/${output_data.id}`} className="btn btn-primary">Read More</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                })}
+                    })}
+                </div>
             </div>
-        </div>
+            <div className="container">
+                <nav aria-label="Page navigation example">
+                    <ul className="pagination">
+                        <li className="page-item"><a className="page-link" href="/">Previous</a></li>
+                        <li className="page-item"><a className="page-link" href="/">1</a></li>
+                        <li className="page-item"><a className="page-link" href="/">2</a></li>
+                        <li className="page-item"><a className="page-link" href="/">3</a></li>
+                        <li className="page-item"><a className="page-link" href="/">Next</a></li>
+                    </ul>
+                </nav>
+            </div>
         </>
     )
 }
