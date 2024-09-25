d00a07aea3f4420311eee5fa33fda8eff8f92eab
Royston-Z <diasroyston034@gmail.com>
2024-09-25 18:32:10 +0530

    change in import strategies

--- src/content/Content.tsx
+++ src/content/Content.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import { Modal } from '../modal';
+import { Modal } from '../Modal';
 
 function Content(props) {
     const [data, setdata] = useState([]);
